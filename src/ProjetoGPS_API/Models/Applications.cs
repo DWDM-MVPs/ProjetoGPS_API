6bad599896a6c266e56c213fa09409094379d62c
Milkenm <miguel.campos.johnny@gmail.com>
2021-06-14 15:00:02 +0100

    Update applications table

--- src/ProjetoGPS_API/Models/Applications.cs
+++ src/ProjetoGPS_API/Models/Applications.cs
@@ -4,8 +4,9 @@
 	{
 		public long ID { get; set; }
 		public string Name { get; set; }
+		public string Surname { get; set; }
 		public string Email { get; set; }
-		public string Address { get; set; }
-		public string Description { get; set; }
+		public string City { get; set; }
+		public string Message { get; set; }
 	}
 }
